cf48530af80864570bba7ebd3e5922bec68e3357
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-12-21 20:43:59 +0200

    Start Login Screen

--- composeApp/src/androidMain/kotlin/org/ninjaneers/motager/PreviewFile.kt
+++ composeApp/src/androidMain/kotlin/org/ninjaneers/motager/PreviewFile.kt
@@ -2,11 +2,14 @@ package org.ninjaneers.motager
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import org.ninjaneers.motager.Authentication.presentation.mainScreen.MainScreen
 import org.ninjaneers.motager.core.presentation.theme.MotagerTheme
 
 @Composable
+@PreviewLightDark()
 @Preview(locale = "ar")
+@Preview(locale = "en")
 fun Preview() {
     MotagerTheme {
         MainScreen()
