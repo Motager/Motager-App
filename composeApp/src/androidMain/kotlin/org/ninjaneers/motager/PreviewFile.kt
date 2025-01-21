0bdcac5f081a5b338bec6c41ea5a2a901655f751
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2025-01-21 21:28:34 +0200

    Finish CollectionsScreen UI + ViewModel

--- composeApp/src/androidMain/kotlin/org/ninjaneers/motager/PreviewFile.kt
+++ composeApp/src/androidMain/kotlin/org/ninjaneers/motager/PreviewFile.kt
@@ -1,15 +1,14 @@
 package org.ninjaneers.motager
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.ninjaneers.motager.core.presentation.theme.MotagerTheme
-import org.ninjaneers.motager.products.presentation.ProductsScreen
 
 @Composable
 @Preview
 fun Preview() {
     MotagerTheme(darkTheme = true) {
-        ProductsScreen()
+
     }
 }
 
--- composeApp/src/commonMain/kotlin/org/ninjaneers/motager/collections/presentation/CollectionsScreenState.kt
+++ composeApp/src/commonMain/kotlin/org/ninjaneers/motager/collections/presentation/CollectionsScreenState.kt
@@ -5,6 +5,12 @@ import org.ninjaneers.motager.collections.domain.Collection
 data class CollectionsScreenState(
     val searchQuery: String = "",
     val searchLimit: Int = 10,
-    val collectionsList: List<Collection> = emptyList(),
+    val collectionsList: List<Collection> = (1..20).map {
+        Collection(
+            name = "Collection 1",
+            productsNumber = 10
+        )
+    },
+    val tableHeaders: List<String> = listOf("Name", "Products", "Actions"),
     val collectionsNumber: Int = collectionsList.size
 )
