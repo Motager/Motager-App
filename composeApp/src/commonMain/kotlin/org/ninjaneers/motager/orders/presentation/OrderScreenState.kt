70e5bea6f5038da0e38fd1e4c4dbecccb86b9631
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2025-01-21 19:49:45 +0200

    Finish OrderScreen StateHoisting

--- composeApp/src/commonMain/kotlin/org/ninjaneers/motager/orders/presentation/OrderScreenState.kt
+++ composeApp/src/commonMain/kotlin/org/ninjaneers/motager/orders/presentation/OrderScreenState.kt
@@ -4,6 +4,13 @@ import org.ninjaneers.motager.orders.domain.Order
 
 data class OrderScreenState(
     val searchQuery: String = "",
+    val tableHeaders: List<String> = listOf(
+        "Order Number",
+        "Customer",
+        "Total",
+        "Status",
+        "Actions"
+    ),
     val searchLimit: Int = 10,
     val ordersList: List<Order> = (1..20).map { Order() },
     val resultNumber: Int = ordersList.size
